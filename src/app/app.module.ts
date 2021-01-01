8b97d4b4b60f26c2fa40d383726fecc09328d01a
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-01-01 12:57:00 +0100

    Add basic masking and reset button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+
+export const options: Partial<IConfig> | (()=>Partial<IConfig>) = {};
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { InputComponent } from './input/input.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot(options)
   ],
   providers: [],
   bootstrap: [AppComponent]
